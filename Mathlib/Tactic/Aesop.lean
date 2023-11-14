33051e1ef18ab51542f22ffea7a8d4094d796366
Scott Morrison <scott.morrison@gmail.com>
2023-11-14 20:07:35 +1100

    Ext is already built in

--- Mathlib/Tactic/Aesop.lean
+++ Mathlib/Tactic/Aesop.lean
@@ -10,24 +10,12 @@ import Std.Tactic.Ext
 /-!
 # Configuration of `aesop` for Mathlib.
 
-We add `ext` and `intro` as unsafe rules (so they can be backtracked if they don't work out).
+We add `intro` as an unsafe rule (so it can be backtracked if it doesn't work out).
 Adding `intro` has the effect of allowing looking inside default transparency definitions for
 lambdas.
 
 We add Mathlib's version of `rfl`.
 -/
-
-namespace Std.Tactic.Ext
-open Lean Elab Tactic
-
-/-- A wrapper for `ext` that we can pass to `aesop`. -/
-def extCore' : TacticM Unit := do
-  evalTactic (← `(tactic| ext))
-
-end Std.Tactic.Ext
-
--- We turn on `ext` inside `aesop`, as an unsafe tactic.
-attribute [aesop unsafe tactic] Std.Tactic.Ext.extCore'
 
 open Lean Elab Tactic in
 def Aesop.intro : TacticM Unit := do evalTactic (← `(tactic| intro))
